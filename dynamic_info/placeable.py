68cf85a5a8edacbd6d23a500f3e2cc22a2b8aceb
Kyle Oliver <oliverke@mail.uc.edu>
2021-09-10 18:39:09 -0400

    Starting on the scripts for getting placeable value for certain

--- dynamic_info/placeable.py
+++ dynamic_info/placeable.py
@@ -8,17 +8,30 @@ import numpy as np
 
 from memory import Hook, ScriptInfo, MissingKwargError
 from pymem import Pymem
+from pymem.ressources.structure import MODULEINFO
 from typing import Union
 
 
 Region = collections.namedtuple('Region', 'xi yi xf yf')
+#var getCursorPositionPointer = ptr("{PlaceableHook.get_cursor_offset}");
+#var getCursorPosition = new NativeFunction(getCursorPositionPointer, "void", pointer);
+#var cursorVector2 = getCursorPosition();
 
 
 def load_placeable_script(session: frida.core.Session, game_assembly: pymem.ressources.structure.MODULEINFO):
     placeable_hook = PlaceableHook(
         address=str(int(game_assembly.lpBaseOfDll + PlaceableHook.offset)),
-        on_enter='send(args[0]);send(args[1]);send(args[2]);send(args[3]);send(args[4]);',
-        on_leave='send(args);',
+        on_enter='''
+        ''',
+        on_leave=f'''
+            var cursorXPointer = cursorVector2.add("0x10")
+            var cursorYPointer = cursorVector2.add("0x14")
+            var x = cursorXPointer.readFloat();
+            var y = cursorYPointer.readFloat();
+            var coordinates = [x, y];
+            send(x);
+        ''',
+        game_assembly=game_assembly,
     )
     placeable_script = session.create_script(str(placeable_hook))
     placeable_script.on('message', placeable_hook.on_message)
@@ -29,10 +42,11 @@ def load_placeable_script(session: frida.core.Session, game_assembly: pymem.ress
 
 class PlaceableHook(Hook, ABC):
     offset = 0x634FA0
+    get_cursor_offset = 0x5D59A0
     loader = load_placeable_script
     script = None
 
-    def __init__(self, address: Union[str, int, None] = None, **kwargs):
+    def __init__(self, address: Union[str, int], game_assembly: MODULEINFO, **kwargs):
         super(PlaceableHook, self).__init__(address, **kwargs)
         self.placeable = False
         self.location = None
@@ -40,27 +54,28 @@ class PlaceableHook(Hook, ABC):
         self.checked_locations = np.full((1080, 1920), False)
         self.placeable_locations = np.full((1080, 1920), False)
         self.btd6 = Pymem('BloonsTD6.exe')
+        self.game_assembly = game_assembly
+
+    def code(self, address) -> str:
+        on_enter_text = self.on_enter_container()
+        on_leave_text = self.on_leave_container()
+        return f'''
+        Interceptor.attach(ptr("{address}"), {{
+            {on_enter_text}
+            {on_leave_text}
+        }});
+        ''' if on_enter_text or on_leave_text else ''
 
     def on_message(self, message, data):
-        if len(message.get('payload')) > 3:
-            read_bytes = self.btd6.read_bytes(message.get('payload').get('rsp'), 8)
-            print(read_bytes)
-            print(self.btd6.read_bytes(int(message.get('payload'), 0), 4))
-            print(message)
-        elif message == 'on_leave':
-            payload = data.get('payload')
-            print(payload)
-            if payload == '0x0':
-                self.placeable = False
-                self.location = mouse.get_position()
-            elif payload == '0x1':
-                self.placeable = True
-                self.location = mouse.get_position()
-            else:
-                self.location = None
+        payload = message.get('payload')
+        if payload == '0x0':
+            self.placeable = False
+            self.location = mouse.get_position()
+        elif payload == '0x1':
+            self.placeable = True
+            self.location = mouse.get_position()
         else:
-            print(message)
-            print(data)
+            self.location = None
 
 
 
